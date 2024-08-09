d52e09e4b22bed3ee0fb6a6596471025d057975a
Patrick van Mourik <patrick@udw.nl>
2024-08-09 13:18:33 +0200

    hosten op github

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,8 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
+  base: '/todoList/', // Zet dit pad naar de naam van je repository
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
